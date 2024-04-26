8656285ac830baa36f7f7574d00c1a6389346655
douaeelh2 <douaeelh2@gmail.com>
2024-04-27 01:01:55 +0200

    Third commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
     };
     return (
         <section id="contact" className="relative">
-            <div className="absolute w-[30%] h-[30%] left-[20%] rounded-full blue__gradient bottom-[100px] behind"/>
+            <div className="absolute w-[30%] h-[30%] left-[60%] rounded-full blue__gradient bottom-[100px] behind"/>
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-sm z-50">
                 <h2 className={`${styles.heading2Center}`}>Have You Any Project ? Please Drop a Message</h2>
                 <p className={`${styles.paragraphCenter}`}>If you like what you see, let's work together.
