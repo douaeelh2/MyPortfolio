00674a09bc21e97487a28e139607642a2df80144
douaeelh2 <douaeelh2@gmail.com>
2024-04-26 23:38:06 +0200

    Third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,9 @@ import styles from "../style";
 const Hero = () => {
     return (
         <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} ${styles.paddingX}`}>
+            <div
+                className="absolute z-[0] w-[35%] h-[35%] md:w-[40%] left-[50%] rounded-full blue__gradient bottom-30"/>
             <div className="grid grid-cols-1 sm:grid-cols-12 place-items-center sm:py-6">
-                <div className="absolute z-[0] w-[35%] h-[35%] md:w-[40%] left-[50%] rounded-full blue__gradient bottom-30" />
 
                 {/* intro section */}
                 <div className="col-span-12 md:col-span-6 lg:col-span-7">
@@ -33,14 +34,15 @@ const Hero = () => {
                     </p>
 
                     <div className="flex items-center flex-col sm:flex-row mt-8">
-                        <Button styles={`hover:scale-[0.99]`} text={`Download CV`} />
+                        <Button styles={`hover:scale-[0.99]`} text={`Download CV`}/>
                     </div>
                 </div>
 
                 {/* image section */}
                 <div className="col-span-12 md:col-span-6 lg:col-span-5 md:w-[70%] lg:w-[75%] h-[100%] hidden md:block">
-                    <div className={`flex-1 flex ${styles.flexCenter} relative overflow-hidden rounded-lg transform rotate-3 feature-img`}>
-                        <img src="/images/DouaeELHILA.jpeg" alt="Hero" className="w-[100%] h-[100%] " />
+                    <div
+                        className={`flex-1 flex ${styles.flexCenter} relative overflow-hidden rounded-lg transform rotate-3 feature-img`}>
+                        <img src="/images/DouaeELHILA.jpeg" alt="Hero" className="w-[100%] h-[100%] "/>
                     </div>
                 </div>
             </div>
