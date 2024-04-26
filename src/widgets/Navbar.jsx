de93efe263e4ee7e17d1c50bbdfccfe20645aba4
douaeelh2 <douaeelh2@gmail.com>
2024-04-27 00:41:44 +0200

    Third commit

--- src/widgets/Navbar.jsx
+++ src/widgets/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className="w-full flex py-6 justify-between items-center navbar">
+    <nav className="w-full flex py-6 justify-between items-center navbar z-50">
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
